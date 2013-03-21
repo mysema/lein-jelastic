23ffa20d655afddf6e7ad26b613ff8ae9af4e237
Lassi Immonen <lassi.immonen@mysema.com>
2013-03-21 15:24:15 +0200

    Add start of authentication

--- src/leiningen/jelastic.clj
+++ src/leiningen/jelastic.clj
@@ -3,30 +3,47 @@
   (:import [com.jelastic JelasticService] 
            [org.apache.tools.ant Project]))
 
-(def ant-project=proxy 
+(def ant-project-proxy 
   (proxy [Project] [] 
     (log [s t] (println s))))
 
 (defn jelastic-service
   [args] 
-  (let [service (JelasticService. ant-project=proxy)]
+  (let [service (JelasticService. ant-project-proxy)]
     (doto service
         (.setApiHoster (:apihoster args))
         (.setContext (:context args))
         (.setEnvironment (:environment args)))
     service))
 
-(defn upload [] )
-(defn deploy [] )
+(defn authenticate
+  [service email password]
+  (let [resp (.authentication service email password)]
+    (if (zero? (.getResult resp)) 
+      resp
+      false)))
+
+(defn upload 
+  [project] 
+  "Upload the current project to Jelastic"
+  (let [conf (:jelastic project)
+        service (jelastic-service conf)]
+    (authenticate service (:email conf) (:password conf))))
+
+
+(defn deploy 
+  [project] 
+  "Deploy the current project to Jelastic")
 
 
 (defn jelastic
-  "Manage jelastic service"
+  "Manage Jelastic service"
   {:help-arglists '([upload deploy])
    :subtasks [#'upload #'deploy]}
   ([project]
    (println (help-for "jelastic")))
   ([project subtask & args]
-   (println "Hi! there")))
+   (case subtask
+     "upload" (apply upload project args))))
 
     
